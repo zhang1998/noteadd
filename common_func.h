92c21b7497c18a24213eae1522e59ebef2b3d2e6
zhang1998 <2764207312@qq.com>
2018-10-01 19:05:16 +0800

    first commit

---
+++ common_func.h
@@ -0,0 +1,7 @@
+#ifndef __COMMON_FUNC_H__
+#define __COMMON_FUNC_H__
+
+#include <gtk/gtk.h>
+GdkPixbuf *create_pixbuf(const gchar * filename);           //？？图标创建
+void toggle_display(GtkWidget *widget, gpointer oneofwidget);   //？？
+#endif // __WINDOW_TEST_H__
---
+++ window_main.h
@@ -0,0 +1,8 @@
+#ifndef __WINDOW_MAIN_H__
+#define __WINDOW_MAIN_H__
+
+#include <gtk/gtk.h>
+#include "common_func.h"
+GtkWidget *create_main_window();        //path=window_main.c
+
+#endif // __WINDOW_MAIN_H__
